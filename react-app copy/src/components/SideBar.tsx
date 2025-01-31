0a4df8aab7f5dc935d7eb438696608fd2367a2fe
David Ghorbani-Nia <d.ghorbani-nia@se22.qmul.ac.uk>
2025-01-31 13:39:56 +0000

    had to change alot

---
+++ react-app copy/src/components/SideBar.tsx
@@ -0,0 +1,41 @@
+import SideBarText from "./SideBarText.tsx";
+export default function SideBar(props) {
+    return (
+        <aside id="closing_sidebar" className="sidebar main_blue_hover_background">
+            <h2 id={"sidebar_header"} className={"sidebar_header"}>
+                <button id="openbtn" onClick={openNav}><i className="fa-solid fa-table-columns"></i></button>
+                Recent
+                <a><i className="fa-regular fa-pen-to-square"></i></a>
+            </h2>
+            <SideBarText text={'GGson'}/>
+            <SideBarText text={'ggson2'}/>
+            <SideBarText text={'ggson3'}/>
+            <SideBarText text={'ggson4sadsadsadsadsdaasasdasdasdasdas'}/>
+            <SideBarText text={'lesgooooo'}/>
+        </aside>
+    )
+
+
+    function openNav() {
+        console.log(document.getElementById("closing_sidebar").style.width);
+        if (document.getElementById("closing_sidebar").style.width == "20%") {
+            document.getElementById("closing_sidebar").style.width = "0";
+            document.getElementById("closing_sidebar").style.padding = "0";
+            document.getElementById("openbtn").style.position = "absolute";
+            document.getElementById("openbtn").style.top = "3rem";
+            document.getElementById("openbtn").style.left = "1rem";
+            document.getElementById("openbtn").style.color = "#00296e";
+        } else {
+            document.getElementById("closing_sidebar").style.width = "20%";
+            document.getElementById("closing_sidebar").style.padding = "1rem";
+            document.getElementById("openbtn").style.position = "relative";
+            document.getElementById("openbtn").style.top = "0";
+            document.getElementById("openbtn").style.left = "0";
+            document.getElementById("openbtn").style.color = "white";
+        }
+    }
+    
+}
+
+
+
---
+++ react-app copy/src/pages/Popup.js
@@ -0,0 +1,61 @@
+import React from "react";
+import './Popup.css';
+import MainFeed from "../components/MainFeed.tsx";
+import InputBar from "../components/InputBar.tsx"
+import {Link} from "react-router-dom";
+
+
+const Popup = () => {
+    return (
+        <div id="placeholder_bg" className="Popup">
+            <link rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
+            <button  id="border-popup-button" onClick={openPopup}>
+                <i className="fa-regular fa-comments"></i>
+            </button>
+            
+            <div id="popup_container" className="main_white_background">
+                <header id="Popup-header" className="main_blue_background header-border-radius">
+                    <div id="Popup-nav-container" className="nav_container">
+                        <div className="sub_nav_container">
+                            <i className="fa-solid fa-house nav_container_item border-top-left" onClick={openPopup}></i>
+                            <p id="header-text">Virtual Assistant</p>
+                        </div>
+
+                        <Link id="popup-link" to="/"><i
+                            className="fa-solid fa-arrow-right nav_container_item border-top-right"></i></Link>
+
+                    </div>
+                </header>
+                <main className="content-popup">
+                    <InputBar/>
+                    <MainFeed/>
+                </main>
+            </div>
+            
+        </div>
+    );
+};
+
+function openPopup() {
+    
+    if(document.getElementById("popup_container").style.height == "35rem")
+    {
+        console.log("isnt 0");
+        console.log(document.getElementById("popup_container").style.height);
+        document.getElementById("border-popup-button").style.display = "inline-block";
+        document.getElementById("popup_container").style.height = "0";
+        document.getElementById("popup_container").style.right = "0";
+        document.getElementById("popup_container").style.bottom = "0";
+    }
+    else
+    {
+        console.log("is 0");
+        document.getElementById("border-popup-button").style.display = "none";
+        document.getElementById("popup_container").style.height = "35rem";
+        document.getElementById("popup_container").style.position = "fixed";
+        document.getElementById("popup_container").style.right = "1rem";
+        document.getElementById("popup_container").style.bottom = "1rem";
+    }
+}
+export default Popup;
